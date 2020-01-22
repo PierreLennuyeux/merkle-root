e21f2709ec759797e6c8da5e1d8a0113e511492b
Pierre Lennuyeux <lennuyeuxpierre@gmail.com>
2020-01-22 10:46:35 +0100

    Suppression des fichiers en trop

--- function.js
+++
@@ -1,9 +0,0 @@
-exports.convertDecimal = function(nbre) { 
-	resultat = parseInt(nbre,16);
-	return resultat;
-};
-
-exports.convertHexa = function(nbre) {
-	resultat = (nbre-0).toString(16);
-	return resultat;
- };
