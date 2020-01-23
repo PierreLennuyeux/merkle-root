abd046fd1641355de23bf4cdf85c9590e8679dfa
Pierre Lennuyeux <lennuyeuxpierre@gmail.com>
2020-01-23 16:10:06 +0100

    Exo 1.1

---
+++ TreeExplorer/Noeud.js
@@ -0,0 +1,9 @@
+class Noeud
+{
+	constructor(valeur) {
+		this.valeur = valeur; this.gauche = undefined; this.droite = undefined; this.parent = undefined;
+	}
+
+}
+
+module.exports = Noeud;
---
+++ TreeExplorer/treeExplorer.js
@@ -0,0 +1,75 @@
+const Arbre = require("./Arbre");
+const Noeud = require("./Noeud");
+
+
+var tree = new Arbre();
+var node = new Noeud();
+
+tree.ajouterNoeud(3);
+// tree.ajouterNoeud(5);
+// tree.afficher();
+
+// class Noeud {
+// 	constructor(valeur) {
+// 		this.valeur = valeur; this.gauche = undefined; this.droite = undefined; this.parent = undefined;
+// 	} 
+	// ajouterNoeud(valeur) {
+	// 		if (valeur < this.valeur) {
+	// 			if (this.gauche === undefined) 
+	// 				{ 
+	// 					this._ajouterNoeud(new Noeud(valeur));
+
+	// 				} 
+	// 				else { 
+	// 					this.gauche.ajouterNoeud(valeur);
+	// 				}
+	// 		} else {
+	// 			if (this.droite === undefined) {
+	// 				this._ajouterNoeud(new Noeud(valeur));
+	// 			} else { 
+	// 				this.droite.ajouterNoeud(valeur);
+	// 			} 
+	// 		}
+	// 	}
+
+	// 	_ajouterNoeud(noeud) {
+	// 		if (noeud.valeur < this.valeur) {
+	// 			this.gauche = noeud; 
+	// 		} else {
+	// 			this.droite = noeud; 
+	// 		}
+	// 		noeud.parent = this; 
+	// 	}
+
+	// 	trouverNoeud(valeur) {
+	// 		if (this.valeur === valeur) {
+	// 			return this;
+	// 		} else if (valeur < this.valeur && this.gauche !== undefined) {
+	// 			return this.gauche.trouverNoeud(valeur);
+	// 		} else if (valeur >= this.valeur && this.droite !== undefined) {
+	// 			return this.droite.trouverNoeud(valeur); }
+	// 			return undefined;
+	// 		}
+	// 	afficher() {
+	// 		var out = "Noeud " + this.valeur + ": L";
+	// 		this.gauche === undefined ? out += "-" : out += this.gauche.valeur; out += " R";
+	// 		this.droite === undefined ? out += "-" : out += this.droite.valeur; out += " P";
+	// 		this.parent === undefined ? out += "-" : out += this.parent.valeur; console.log(out);
+
+	// 		if (this.gauche !== undefined) 
+	// 			this.gauche.afficher();
+
+	// 		if (this.droite !== undefined) 
+	// 			this.droite.afficher();
+//	// 	}
+// }
+
+// class Arbre {
+// 	constructor() {
+// 		this.racine = undefined; 
+// 	}
+// }
+
+
+
+
